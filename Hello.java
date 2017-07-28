48da9d9d5f2fe4f46b36072ee735471321d8bbc9
DeanMC <dean_mcarthur@mentor.com>
2017-07-28 00:11:42 -0400

    Expanded Refinements
    Add Book end statements to bracket the processing loop

--- Hello.java
+++ Hello.java
@@ -3,9 +3,11 @@
 public class Hello {
 
   public static void main(String[] args) {
+    System.out.println("Beginning of Session");
          for( int i=1;i<=25;i++) {
    		System.out.println( "Hello World ... " + i);
           }
+    System.out.println("End of Session");
   }
 
 }
